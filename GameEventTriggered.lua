63c56f23eb6ac74a60bc7993691327ad8edb31ab
negbook <negbook@gmail.com>
2021-02-22 00:01:07 +0800

    Add files via upload

--- GameEventTriggered.lua
+++ GameEventTriggered.lua
@@ -1,17 +1,11 @@
-local IsUntilForLocalUse = false 
+local ThisIsUtilForLocalScript = true 
 
 AddEventHandler('gameEventTriggered',function(name,args)
     GameEventTriggered(name,args)
 end)
 
-if IsUntilForLocalUse then 
-    TriggerEvent = function(func,...)
-        local a = {...}
-        _G[func](...)
-    end 
-end 
-
 function GameEventTriggered(eventName, data)
+
     if eventName == "CEventNetworkEntityDamage" then
         victim = tonumber(data[1])
         attacker = tonumber(data[2])
@@ -27,46 +21,89 @@ function GameEventTriggered(eventName, data)
         if victim and attacker then
             if victimDied then
                 if IsEntityAVehicle(victim) then
-                    TriggerEvent("OnVehicleDestroyed", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag);
-
+                    if not ThisIsUtilForLocalScript then 
+                        TriggerEvent("OnVehicleDestroyed", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                    elseif OnVehicleDestroyed then 
+                        OnVehicleDestroyed(victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                    end 
                 else
 
                     if IsEntityAPed(victim) then
                         if IsEntityAVehicle(attacker) then
-                            TriggerEvent("OnPedKilledByVehicle", victim, attacker, weaponHash);
+                            if not ThisIsUtilForLocalScript then 
+                                TriggerEvent("OnPedKilledByVehicle", victim, attacker, weaponHash)
+                            elseif OnPedKilledByVehicle then  
+                                OnPedKilledByVehicle(victim, attacker, weaponHash)
+                            end 
+                            
                         elseif IsEntityAPed(attacker)  then
                             if IsPedAPlayer(attacker) then
                                 player = NetworkGetPlayerIndexFromPed(attacker);
                                 if IsPedAPlayer(victim) then
-                                    TriggerEvent("OnPlayerKilledByPlayer", NetworkGetPlayerIndexFromPed(victim), NetworkGetPlayerIndexFromPed(attacker), weaponHash, isMeleeDamage);
+                                    if not ThisIsUtilForLocalScript then 
+                                        TriggerEvent("OnPlayerKilledByPlayer", NetworkGetPlayerIndexFromPed(victim), NetworkGetPlayerIndexFromPed(attacker), weaponHash, isMeleeDamage)
+                                    elseif OnPlayerKilledByPlayer then   
+                                        OnPlayerKilledByPlayer(NetworkGetPlayerIndexFromPed(victim), NetworkGetPlayerIndexFromPed(attacker), weaponHash, isMeleeDamage)
+                                    end 
                                 else 
-                                     TriggerEvent("OnPedKilledByPlayer", victim, player, weaponHash, isMeleeDamage);
+                                    if not ThisIsUtilForLocalScript then 
+                                        TriggerEvent("OnPedKilledByPlayer", victim, player, weaponHash, isMeleeDamage)
+                                    elseif OnPedKilledByPlayer then   
+                                        OnPedKilledByPlayer(victim, player, weaponHash, isMeleeDamage)
+                                    end 
                                     
                                 end 
                             else
                                 if IsPedAPlayer(victim) then
-                                    TriggerEvent("OnPlayerKilledByPed", NetworkGetPlayerIndexFromPed(victim), attacker, weaponHash, isMeleeDamage);
+                                    if not ThisIsUtilForLocalScript then 
+                                    TriggerEvent("OnPlayerKilledByPed", NetworkGetPlayerIndexFromPed(victim), attacker, weaponHash, isMeleeDamage)
+                                    elseif OnPlayerKilledByPed then    
+                                    OnPlayerKilledByPed(NetworkGetPlayerIndexFromPed(victim), attacker, weaponHash, isMeleeDamage)
+                                    end 
                                 else
-                                    TriggerEvent("OnPedKilledByPed",victim, attacker, weaponHash, isMeleeDamage);
+                                    if not ThisIsUtilForLocalScript then 
+                                        TriggerEvent("OnPedKilledByPed",victim, attacker, weaponHash, isMeleeDamage)
+                                    elseif OnPedKilledByPed then   
+                                        OnPedKilledByPed(victim, attacker, weaponHash, isMeleeDamage)
+                                    end 
                                 end
                             end
                         else
                             if IsPedAPlayer(victim) then
-                                TriggerEvent("OnPlayerDied", NetworkGetPlayerIndexFromPed(victim), attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag);
+                                if not ThisIsUtilForLocalScript then 
+                                    TriggerEvent("OnPlayerDied", NetworkGetPlayerIndexFromPed(victim), attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                                elseif OnPlayerDied then    
+                                    OnPlayerDied(NetworkGetPlayerIndexFromPed(victim), attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                                end 
                             end
                         end
-                        TriggerEvent("OnPedDied", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag);
- 
+                        if not ThisIsUtilForLocalScript then 
+                            TriggerEvent("OnPedDied", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                        elseif OnPedDied then     
+                            OnPedDied(victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                        end 
                     else
-                        TriggerEvent("OnEntityKilled", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag);
+                        if not ThisIsUtilForLocalScript then 
+                            TriggerEvent("OnEntityKilled", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                        elseif OnEntityKilled then     
+                            OnEntityKilled(victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                        end 
                     end
                 end
             else
 
                 if not IsEntityAVehicle(victim)  then
-                    TriggerEvent("OnEntityDamaged", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag);
+                    if not ThisIsUtilForLocalScript then 
+                        TriggerEvent("OnEntityDamaged", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                    elseif OnEntityDamaged then  
+                        OnEntityDamaged(victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                    end 
                 else
-                    TriggerEvent("OnVehicleDamaged", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag);
+                    if not ThisIsUtilForLocalScript then 
+                        TriggerEvent("OnVehicleDamaged", victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                    elseif OnVehicleDamaged then   
+                        OnVehicleDamaged(victim, attacker, weaponHash, isMeleeDamage, vehicleDamageTypeFlag)
+                    end 
                 end
             end
         end
